f931837008741f5f726ebb2ba9f9148d139fd7e9
Grant Palin <grant@grantpalin.com>
2017-04-06 19:25:26 -0700

    Amended post formats support registration.

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function fxdchld_setup() {
 
 	// override parent theme setting for post format support
 	// options: aside audio chat gallery image link quote status video
-	add_theme_support( 'post-formats', array( 'gallery', 'image', 'link', 'quote', 'status', 'video' ) );
+	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'image', 'link', 'quote', 'status', 'video' ) );
 }
 add_action( 'after_setup_theme', 'fxdchld_setup', 20 ); // should execute after parent theme setup; default is 10
 
